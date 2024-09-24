594188912e684715ae5f91d5d44f1f75ad008722
Axell505 <axelcrbl505@hotmail.com>
2024-09-23 23:57:50 -0300

    Vistas creadas junto con el formulario.

--- blog/post/views.py
+++ blog/post/views.py
@@ -1,3 +1,50 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404, redirect
+from django.contrib.auth.decorators import login_required
+from .models import Post
+from .forms import PostForm
 
-# Create your views here.
+# Listar todas las publicaciones
+def post_list(request):
+    posts = Post.objects.all()
+    return render(request, 'post_list.html', {'posts': posts})
+
+# Crear una nueva publicación
+@login_required
+def post_create(request):
+    if request.method == 'POST':
+        form = PostForm(request.POST)
+        if form.is_valid():
+            post = form.save(commit=False)
+            post.author = request.user
+            post.save()
+            return redirect('post_list')
+    else:
+        form = PostForm()
+    return render(request, 'post_form.html', {'form': form, 'action': 'Crear Publicación'})
+
+# Ver el detalle de una publicación específica
+def post_detail(request, id):
+    post = get_object_or_404(Post, id=id)
+    return render(request, 'post_detail.html', {'post': post})
+
+# Editar una publicación existente
+@login_required
+def post_edit(request, id):
+    post = get_object_or_404(Post, id=id)
+    if request.method == 'POST':
+        form = PostForm(request.POST, instance=post)
+        if form.is_valid():
+            form.save()
+            return redirect('post_detail', id=post.id)
+    else:
+        form = PostForm(instance=post)
+    return render(request, 'post_form.html', {'form': form, 'action': 'Editar Publicación'})
+
+# Eliminar una publicación
+@login_required
+def post_delete(request, id):
+    post = get_object_or_404(Post, id=id)
+    if request.method == 'POST':
+        post.delete()
+        return redirect('post_list')
+    return render(request, 'post_confirm_delete.html', {'post': post})
